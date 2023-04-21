6d41a17d963f9f7dc97f1f02719428b2e0235d3e
ErickFerrer96 <erick.ferrer1996@gmail.com>
2023-04-20 19:02:20 -0600

    Test added

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+""" Sample Test"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module"""
+
+    def test_add_numbers(self):
+        """Testing adding number together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """Testing subtract numbers"""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res,5)
